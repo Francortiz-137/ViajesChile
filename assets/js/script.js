0a6089f46659d98a1c4a17624e4b4de217309ea1
Francortiz-137 <francortiz.c@gmail.com>
2024-03-15 20:45:42 -0300

    added tooltips

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,3 @@
-
-$(document).ready(function() {
-    //enable tooltip
-    $('[data-toggle="tooltip"]').tooltip();
-
-    console.log("Hi");
-    // Event listener for tooltip show event
-    $('#myButton').on('shown.bs.tooltip', function () {
-        console.log('Tooltip shown');
-        // Here you can perform any actions you need when the tooltip is triggered
-    });
-
-    // Event listener for tooltip hide event
-    $('#myButton').on('hidden.bs.tooltip', function () {
-        console.log('Tooltip hidden');
-        // Here you can perform any actions you need when the tooltip is hidden
-    });
-});
-
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
